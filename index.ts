d48f6e4a51021d482f2197e6a26b87d7cd318941
Eklavya Singh <eklavyasingh12065@gmail.com>
2022-10-04 13:32:23 +0530

    fixed cors

--- index.ts
+++ index.ts
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: process.env.ALLOWED_DOMAINS,
         optionsSuccessStatus: 200,
     })
 );
