80d458a58046f6cd9d0e1c11a7e0683c16f75e22
Eklavya Singh <eklavyasingh12065@gmail.com>
2022-10-10 16:34:35 +0530

    added cors

--- index.ts
+++ index.ts
@@ -23,7 +23,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: ["http://localhost:3000", "https://admin.dbzbeds.co.uk"],
         optionsSuccessStatus: 200,
     })
 );
