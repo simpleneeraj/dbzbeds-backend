223a00bb7a2efa3a5017c36e2404ba46411fd5ad
Aasif Alvi <Asif.Alvi@epicglobal.tech>
2022-11-10 15:49:35 +0530

    fileUploadController

--- controllers/file-upload-controllers.ts
+++ controllers/file-upload-controllers.ts
@@ -2,25 +2,58 @@ import { Request, Response, NextFunction } from "express";
 import multiplefile from "../models/multiplefile";
 import multipleFileUpload from "../services/image-service";
 
-
+{/* upload multiple beds */ }
 export const UploadMutipleImageController = async (req: Request, res: Response) => {
     try {
         const { name, price, des } = req.body;
         if (!name || !price || !des) {
             res.send(400).send("all fields are required {name,price,dec}")
         }
-        const response = await multipleFileUpload(req.files, { name, price, des });
-        res.status(200).send(response);
-    } catch (error) {
-        res.status(400).send(error)
+        const uploadbeds = await multipleFileUpload(req.files, { name, price, des });
+        res.status(200).send(uploadbeds);
+    } catch (error: any) {
+        res.status(400).json({ error: error.message });
     }
 }
 
-export const getallMultipleFiles = async (req: Request, res: Response) => {
+{/* get all multiple beds */ }
+export const getAllMultipleFiles = async (req: Request, res: Response) => {
     try {
         const response = await multiplefile.find();
         res.status(200).send(response);
-    } catch (error) {
-     res.status(400).send(error);   
+    } catch (error: any) {
+        res.status(400).json({ error: error.message });
+    }
+}
+
+{/* get multiple beds by id*/ }
+export const getAllMultipleFilesById = async (req: Request, res: Response) => {
+    try {
+        const response = await multiplefile.findById(req.params.id);
+        res.status(200).json(response);
+    } catch (error: any) {
+        res.status(400).json({ error: error.message });
+    }
+}
+
+{/* delete multiple beds by id*/ }
+export const deleteMultipleFiles = async (req: Request, res: Response) => {
+    try {
+        await multiplefile.findByIdAndDelete(req.params.id);
+        res.status(200).json("beds has been deleted");
+    } catch (error: any) {
+        res.status(400).json({ error: error.message });
+    }
+}
+
+{/* update beds by id*/ }
+export const updateMultipleFiles = async (req: Request, res: Response) => {
+    try {
+        const updateFiles = await multiplefile.findByIdAndUpdate(
+            req.params.id, { $set: req.body }, { new: true }
+        );
+        res.status(200).json(updateFiles);
+    } catch (error: any) {
+        res.status(400).json({ error: error.message });
     }
 }
--- routes/fileroutes.ts
+++ routes/fileroutes.ts
@@ -1,13 +1,28 @@
 import { Router } from "express";
 import upload from "../config/multer";
-import { getallMultipleFiles, UploadMutipleImageController } from "../controllers/file-upload-controllers";
+import {
+    deleteMultipleFiles,
+    getAllMultipleFiles,
+    getAllMultipleFilesById,
+    updateMultipleFiles,
+    UploadMutipleImageController
+} from "../controllers/file-upload-controllers";
 
 const router = Router();
 
-// uploading multiple images..
+// uploading multiple beds..
 router.post('/multipleFiles', upload.array('files'), UploadMutipleImageController);
 
-//get all multiple images..
-router.get('/getMultipleFiles', getallMultipleFiles);
+//get all multiple beds..
+router.get('/getMultipleFiles', getAllMultipleFiles);
+
+//get all beds with multiple images by id
+router.get('/getMultipleFiles/:id', getAllMultipleFilesById);
+
+// delete beds by id
+router.delete('/deleteMultipleFiles/:id', deleteMultipleFiles);
+
+// update beds by id
+router.put('/updateMultipleFiles/:id', updateMultipleFiles);
 
 export default router;
